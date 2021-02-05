c5f15becdec09968a129a0ff8c72fbe992e80322
zigliad <lilizigi@gmail.com>
2021-02-05 18:21:03 +0200

    Added routing and responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import Navbar from "Components/navbar";
+import Routing from "Components/Routing/Routing";
+import 'styles/main.css';
 
 function App() {
 	return (
-		<Navbar/>
+		<div className="h-screen bg-gray-100">
+			<Routing/>
+		</div>
 		// <div className="flex flex-col items-center justify-center h-screen bg-white">
 		// 	<div className="h-96 w-2/3 bg-gradient-to-r from-green-400 to-green-500  rounded-3xl">
 		// 		<div className="flex flex-col items-center justify-center h-full w-full bg-gray-100 rounded-3xl transform -rotate-2">
---
+++ src/Config/config.js
@@ -0,0 +1,46 @@
+import MainPage from "Components/Pages/MainPage";
+import AboutUs from "Components/Pages/AboutUs";
+import OurProducts from "Components/Pages/OurProducts";
+import OurTeam from "Components/Pages/OurTeam";
+
+const COMPANY_NAME = "KANASOFT"
+
+const LINKS_DATA = {
+
+    about: {
+        to: "/about",
+        title: "About Us",
+        component: AboutUs
+    },
+
+    products: {
+        to: "/products",
+        title: "Our Products",
+        component: OurProducts
+    }, 
+    
+    team: {
+        to: "/team",
+        title: "Our Team",
+        component: OurTeam 
+    },
+
+    // Keep last!
+    main: {
+        to: "/",
+        title: "Main",
+        component: MainPage
+    }
+}
+
+const NAV_BAR_DATA = [
+    LINKS_DATA.about,
+    LINKS_DATA.products,
+    LINKS_DATA.team,
+]
+
+export {
+    COMPANY_NAME,
+    NAV_BAR_DATA,
+    LINKS_DATA
+}
