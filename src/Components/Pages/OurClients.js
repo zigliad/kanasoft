a19926cc6d34d736de0a0a461eaa765e25aa204c
Liad Zigdon <liadzigdon@Liads-MBP.lan>
2021-03-22 23:37:57 +0200

    Added clients info

--- src/Components/Pages/OurClients.js
+++ src/Components/Pages/OurClients.js
@@ -3,26 +3,17 @@ import { UndrawExperts } from 'react-undraw-illustrations';
 import PageHeader from 'Components/Other/PageHeader';
 import { CLIENTS_DATA } from 'Config/clients';
 import { useState } from 'react';
+import PureModal from 'react-pure-modal';
+import 'react-pure-modal/dist/react-pure-modal.min.css';
 
-function ClientCard({ client }) {
-
-    const [open, setOpen] = useState(false)
-
-    const toggle = () => {
-        setOpen(!open)
-    }
+function ClientCard({ client, onClick }) {
 
     return (
-
         // Use in case of additional info:
-        <button onClick={toggle} className={"p-4 shadow-lg rounded-lg hover:shadow-xl transition " + (client.darkBg ? "bg-gray-700" : "bg-gray-200")}>
+        <button onClick={() => onClick(client)} className={"p-4 shadow-lg rounded-lg hover:shadow-xl transition " + (client.darkBg ? "bg-gray-700" : "bg-gray-200")}>
             <div className={"flex flex-col items-center space-y-4"}>
                 <img src={client.image} className="max-w-24 h-24 object-fit" />
                 <p className={"text-xl font-medium " + (client.darkBg ? "text-gray-200" : "text-gray-600")}>{client.name}</p>
-                {
-                    open && client.info &&
-                    <p className={"text-lg font-medium " + (client.darkBg ? "text-white" : "text-black")}>{client.info}</p>
-                }
             </div>
         </button>
 
@@ -38,7 +29,17 @@ function ClientCard({ client }) {
 
 export default function OurClients() {
 
-    const data = Object.values(CLIENTS_DATA).map((client, index) => <ClientCard client={client} key={index} />);
+    const [modalOpen, setModalOpen] = useState(false)
+    const [chosenClient, setChosenClient] = useState({})
+
+    const clientClicked = (client) => {
+        if (client.info) {
+            setChosenClient(client)
+            setModalOpen(true)
+        }
+    }
+
+    const data = Object.values(CLIENTS_DATA).map((client, index) => <ClientCard client={client} onClick={clientClicked} key={index} />);
 
     return (
         <div className="page-padding">
@@ -51,6 +52,19 @@ export default function OurClients() {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8">
                 {data}
             </div>
+            <PureModal
+                isOpen={modalOpen}
+                onClose={() => {
+                    setModalOpen(false)
+                    return true
+                }}
+                width="500px"
+            >
+                <div className="flex flex-col p-8 space-y-4">
+                    <p className="gradient-title">{chosenClient.longName || chosenClient.name}</p>
+                    <p className="info-text">{chosenClient.info}</p>
+                </div>
+            </PureModal>
         </div>
     );
 }
