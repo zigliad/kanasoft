11087342efac4b5981ad5969050df1a1de0e1179
Liad Zigdon <liadzigdon@Liads-MBP.lan>
2021-07-03 09:40:16 +0300

    Add doctor title to stefania

--- src/Config/team.js
+++ src/Config/team.js
@@ -1,30 +1,30 @@
-import AvnerTal from "Images/Team/AvnerTal.jpg"
-import BDO from "Images/Team/BDO.jpg"
-import DanRothberg from "Images/Team/DanRothberg.jpg"
-import GabiKabazo from "Images/Team/GabiKabazo.jpg"
-import GilShapira from "Images/Team/GilShapira.jpg"
-import HaroldWolkin from "Images/Team/HaroldWolkin.jpg"
-import MotiMaram from "Images/Team/MotiMaram.jpg"
-import RomanBrenner from "Images/Team/RomanBrenner.jpg"
-import StefaniaSzabo from "Images/Team/StefaniaSzabo.jpg"
-import YiftahBenYaackov from "Images/Team/YiftahBenYaackov.jpg"
+import AvnerTal from "Images/Team/AvnerTal.jpg";
+import BDO from "Images/Team/BDO.jpg";
+import DanRothberg from "Images/Team/DanRothberg.jpg";
+import GabiKabazo from "Images/Team/GabiKabazo.jpg";
+import GilShapira from "Images/Team/GilShapira.jpg";
+import HaroldWolkin from "Images/Team/HaroldWolkin.jpg";
+import MotiMaram from "Images/Team/MotiMaram.jpg";
+import RomanBrenner from "Images/Team/RomanBrenner.jpg";
+import StefaniaSzabo from "Images/Team/StefaniaSzabo.jpg";
+import YiftahBenYaackov from "Images/Team/YiftahBenYaackov.jpg";
 
 const TEAM_DATA = [
-    {
-        name: "Moti Maram",
-        job: "President and Director",
-        info: `
+	{
+		name: "Moti Maram",
+		job: "President and Director",
+		info: `
         In 1986, together with a partner company, he founded a computer company that mainly provided services to
         EXATEC-compliant IBM AS 400 computers and subsequently sales and service to the first PCs in the world.
         After identifying a need for CRM and ERP software, Maram and his partner, Avner Tal,
         established a software company that manufactures CRM and ERP management software called BENEFIT in 2000.
         `,
-        image: MotiMaram
-    },
-    {
-        name: "Yiftah Ben Yaackov",
-        job: "CEO and Director",
-        info: `
+		image: MotiMaram,
+	},
+	{
+		name: "Yiftah Ben Yaackov",
+		job: "CEO and Director",
+		info: `
             Professional lawyer since 2004. Specialized in real estate related issues on all
             aspects of it including real estate corporations, complex sales transactions,
             real estate unions, real estate taxation. Notary License holder since 2014.
@@ -33,79 +33,79 @@ const TEAM_DATA = [
             of 2 of the largest medical cannabis company's in Israel and has an enormous
             experience and knowledge in this field.
         `,
-        image: YiftahBenYaackov
-    },
-    {
-        name: "Avner Tal",
-        job: "VP Sales & Marketing, CTO and Director",
-        info: `
+		image: YiftahBenYaackov,
+	},
+	{
+		name: "Avner Tal",
+		job: "VP Sales & Marketing, CTO and Director",
+		info: `
             Technology director of Beyond Solution Company. I studied electronics and control.
             I served in the Navy and I was in the team that implemented and developed the technology
             and even carried out the experiments that included "smart" missiles. Joined Moti to
             established a computer company that supported Exatech computers and its innovative network.
         `,
-        image: AvnerTal,
-    },
-    {
-        name: "Gabi Kabazo",
-        job: "CFO and Director",
-        info: `
+		image: AvnerTal,
+	},
+	{
+		name: "Gabi Kabazo",
+		job: "CFO and Director",
+		info: `
         Gabi is a seasoned finance and operations professional with over 20 years’ experience 
         supporting accounting, financing and IT operations in complex corporate settings. 
         Since 2009, Mr. Kabazo has been with TELUS Telecommunications Company. From 2002-2011
         he served as CFO for m-Wise Inc. He received B.A from TAU and M.B.A from UBC in Accounting & Economics.
         `,
-        image: GabiKabazo
-    },
-    {
-        name: "Stefánia Szabó",
-        job: "Director",
-        info: `
+		image: GabiKabazo,
+	},
+	{
+		name: "Dr. Stefánia Szabó",
+		job: "Director",
+		info: `
         Stefánia is a former diplomat, a charismatic government services, management and public relations professional,
         with skills in all areas of bilateral and multilateral relationship management, diplomacy, trade and HR honed over
         25 years of progressive experience. She is an international lawyer by profession, and serves as member of the executive
         committee of the Canada-Israel Chamber of Commerce.
         `,
-        image: StefaniaSzabo
-    },
-    // {
-    //     name: "Roman Brenner",
-    //     job: "Director",
-    //     info: `
-    //         Retired business executive. Managed several manufacturing operations for 
-    //         public and private companies operating in the NAFTA region and internationally.
-    //         Experience in international trade. Sat on Advisory Boards for private companies
-    //         and Government Advisory Boards.
-    //     `,
-    //     image: RomanBrenner
-    // },
-    {
-        name: "Harold Wolkin",
-        job: "Director",
-        info: `
+		image: StefaniaSzabo,
+	},
+	// {
+	//     name: "Roman Brenner",
+	//     job: "Director",
+	//     info: `
+	//         Retired business executive. Managed several manufacturing operations for
+	//         public and private companies operating in the NAFTA region and internationally.
+	//         Experience in international trade. Sat on Advisory Boards for private companies
+	//         and Government Advisory Boards.
+	//     `,
+	//     image: RomanBrenner
+	// },
+	{
+		name: "Harold Wolkin",
+		job: "Director",
+		info: `
         Harold is a retired, former Investment Banker and Financial Analyst with over 40 years
         of business experience. An advisor to Chief Executive Officers and Board of Directors of
         both public and private companies. Strong financial expertise, a Chartered Financial Analyst
         (CFA) and member of the Institute of Corporate Directors.
         `,
-        image: HaroldWolkin
-    },
-    {
-        name: "Daniel Rothberg",
-        job: "Corporate Secretary",
-        info: `
+		image: HaroldWolkin,
+	},
+	{
+		name: "Daniel Rothberg",
+		job: "Corporate Secretary",
+		info: `
         Partner at Devry Smith Frank LLP, a full-service law firm in Toronto, Canada. Mr. Rothberg graduated
         with a Bachelor of Commerce (Hons.), from the University of Manitoba majoring in finance and earned his
         law degree from the UBC in 1993. Mr. Rothberg has been practicing Corporate Finance and Securities Law
         for over 25 years, providing legal assistance both to private enterprise and publicly traded companies
         listed on the TSX, TSXV. CSE and the OTC markets.
         `,
-        image: DanRothberg
-    },
-    {
-        name: "Gil Shapira",
-        job: "Greenhouse Facility Project Manager",
-        info: `
+		image: DanRothberg,
+	},
+	{
+		name: "Gil Shapira",
+		job: "Greenhouse Facility Project Manager",
+		info: `
         Engineer Gil Shapira brings 30 years of prior experience in planning, managing, and implementing complex
         engineering projects. Operating both locally and internationally, Gil continues to provide customized services
         to the pharmaceutical, biotechnological, medical device, health and food industries. Thousands of square meters
@@ -114,19 +114,17 @@ const TEAM_DATA = [
         finally handover.  All have been certified by the relevant authorities, such as the FDA, EMEA, GMP, GLP, FACT,
         NETCORD, JACIE, and the Israeli Ministry of Health.
         `,
-        image: GilShapira
-    },
-    {
-        name: "BDO Israel",
-        job: "Advisors",
-        info: `
+		image: GilShapira,
+	},
+	{
+		name: "BDO Israel",
+		job: "Advisors",
+		info: `
         BDO Israel, one of the top accounting and consulting firms in Israel,
         has been the consulting accompanying the Beyond-Cannasoft company.
         `,
-        image: BDO
-    },
-]
+		image: BDO,
+	},
+];
 
-export {
-    TEAM_DATA,
-}
+export { TEAM_DATA };
