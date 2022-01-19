9606e47065bfaf2863dfcb743f20d1172a69abc9
Liad Zigdon <liadzigdon@Liads-MBP.lan>
2022-01-19 21:15:49 +0200

    News presses are now loaded dynamically

--- src/Components/Other/News.js
+++ src/Components/Other/News.js
@@ -1,121 +1,128 @@
-import 'styles/main.css';
-import { useState, useEffect } from 'react';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import { NEWS_URI } from 'Config/config';
-import axios from 'axios';
+import "styles/main.css";
+import { useState, useEffect } from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import {
+	DOCUMENT_CATEGORIES,
+	NEWS_URI,
+	YAHOO_FINANCE_PRESSES,
+} from "Config/config";
+import axios from "axios";
 
 function NewsCard(props) {
-    const { title, message, link, imgUri } = props.data
-    return (
-        <div className="bg-gray-200 rounded-xl shadow-lg w-full max-h-96 overflow-auto relative">
-            {
-                imgUri &&
-                <div className="h-44 rounded-t-xl">
-                    <img src={imgUri}
-                        className="max-w-full max-h-full min-w-full min-h-full rounded-t-xl object-cover" />
-                </div>
-            }
-            <div className="p-8">
-                {
-                    link &&
-                    <div className="mb-6">
-                        <a target="_blank" href={link}
-                            className="no-underline hover:underline text-ks-purple font-medium">
-                            Read the article
-                        </a>
-                    </div>
-                }
-                {
-                    title &&
-                    <p className="text-2xl font-medium">{title}</p>
-                }
-                {
-                    message &&
-                    <p className="text-lg mt-2">{message}</p>
-                }
-            </div>
-        </div>
-    )
+	const { title, message, link, imgUri } = props.data;
+	return (
+		<div className="bg-gray-200 rounded-xl shadow-lg w-full max-h-96 overflow-auto relative">
+			{imgUri && (
+				<div className="h-44 rounded-t-xl">
+					<img
+						src={imgUri}
+						className="max-w-full max-h-full min-w-full min-h-full rounded-t-xl object-cover"
+					/>
+				</div>
+			)}
+			<div className="p-8">
+				{link && (
+					<div className="mb-6">
+						<a
+							target="_blank"
+							href={link}
+							className="no-underline hover:underline text-ks-purple font-medium"
+						>
+							Read the article
+						</a>
+					</div>
+				)}
+				{title && <p className="text-2xl font-medium">{title}</p>}
+				{message && <p className="text-lg mt-2">{message}</p>}
+			</div>
+		</div>
+	);
 }
 
 export default function News() {
+	const [loading, setLoading] = useState(true);
+	const [data, setData] = useState([]);
 
-    const [loading, setLoading] = useState(true)
-    const [data, setData] = useState([])
+	const responsive = {
+		desktop: {
+			breakpoint: { max: 3000, min: 1024 },
+			items: 3,
+		},
+		tablet: {
+			breakpoint: { max: 1024, min: 464 },
+			items: 1,
+		},
+		mobile: {
+			breakpoint: { max: 464, min: 0 },
+			items: 1,
+		},
+	};
 
-    const responsive = {
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3,
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 1,
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1,
-        }
-    };
+	const newsCards = data.map((news, index) => (
+		<NewsCard data={news} key={index} />
+	));
 
-    const newsCards = data.map((news, index) => <NewsCard data={news} key={index} />)
+	useEffect(() => {
+		const init = async () => {
+			setLoading(true);
+			const result = await axios.get(YAHOO_FINANCE_PRESSES);
+			if (result && result.data) {
+				const finalData = result.data.list
+					.filter(
+						(record) =>
+							DOCUMENT_CATEGORIES.indexOf(
+								record.document_category
+							) >= 0
+					)
+					.map((record) => {
+						return {
+							title: "Continuous Disclosure",
+							link: record.url,
+							message: record.public_date,
+						};
+					});
+				setData(finalData);
+			}
+			setLoading(false);
+		};
+		init();
+	}, []);
 
-    useEffect(() => {
-        const init = async () => {
-            setLoading(true)
-            axios({
-                url: NEWS_URI,
-                method: 'GET',
-                responseType: 'blob', // important
-            }).then((response) => {
-                response.data.text().then(text => {
-                    try {
-                        setData(JSON.parse(text))
-                    } catch { }
-                    setLoading(false)
-                })
-            });
-        }
-        init()
-    }, [])
-
-    if (loading) {
-        return (
-            <div></div>
-        );
-    } else {
-        return (
-            <div className="">
-                {
-                    data.length === 0 &&
-                    <div className="w-full flex flex-col items-center">
-                        <p className="text-3xl font-medium text-center">Oops</p>
-                        <p className="text-2xl font-medium text-center">We don't have any press releases yet, but please keep in touch!</p>
-                    </div>
-                }
-                {
-                    data.length > 0 &&
-                    <Carousel
-                        swipeable={true}
-                        draggable={true}
-                        showDots={true}
-                        responsive={responsive}
-                        infinite={true}
-                        autoPlay={true}
-                        autoPlaySpeed={4000}
-                        keyBoardControl={true}
-                        transitionDuration={500}
-                        containerClass="carousel-container"
-                        removeArrowOnDeviceType={["tablet", "mobile"]}
-                        dotListClass=""
-                        itemClass="p-2 md:p-6 mb-8"
-                    >
-                        {newsCards}
-                    </Carousel>
-
-                }
-            </div>
-        );
-    }
+	if (loading) {
+		return <div></div>;
+	} else {
+		return (
+			<div className="">
+				{data.length === 0 && (
+					<div className="w-full flex flex-col items-center">
+						<p className="text-3xl font-medium text-center">Oops</p>
+						<p className="text-2xl font-medium text-center">
+							We don't have any press releases yet, but please
+							keep in touch!
+						</p>
+					</div>
+				)}
+				{data.length > 0 && (
+					<Carousel
+						swipeable={true}
+						draggable={true}
+						showDots={true}
+						responsive={responsive}
+						infinite={true}
+						autoPlay={true}
+						autoPlaySpeed={4000}
+						keyBoardControl={true}
+						transitionDuration={500}
+						containerClass="carousel-container"
+						removeArrowOnDeviceType={["tablet", "mobile"]}
+						dotListClass=""
+						itemClass="p-2 md:p-6 mb-8"
+					>
+						{newsCards}
+					</Carousel>
+				)}
+			</div>
+		);
+	}
 }
--- src/Config/config.js
+++ src/Config/config.js
@@ -7,80 +7,84 @@ import PressReleases from "Components/Pages/PressReleases";
 import StockPage from "Components/Pages/StockPage";
 import OurClients from "Components/Pages/OurClients";
 
-const COMPANY_NAME = "CANNASOFT"
+const COMPANY_NAME = "CANNASOFT";
 
 const LINKS_DATA = {
+	about: {
+		to: "/about",
+		title: "About",
+		component: AboutUs,
+	},
 
-    about: {
-        to: "/about",
-        title: "About",
-        component: AboutUs
-    },
+	team: {
+		to: "/team",
+		title: "Team",
+		component: OurTeam,
+	},
 
-    team: {
-        to: "/team",
-        title: "Team",
-        component: OurTeam
-    },
+	products: {
+		to: "/products",
+		title: "Products",
+		component: OurProducts,
+	},
 
-    products: {
-        to: "/products",
-        title: "Products",
-        component: OurProducts
-    },
+	contact: {
+		to: "/contact",
+		title: "Contact",
+		component: ContactUs,
+	},
 
-    contact: {
-        to: "/contact",
-        title: "Contact",
-        component: ContactUs
-    },
+	press: {
+		to: "/press",
+		title: "Press",
+		component: PressReleases,
+	},
 
-    press: {
-        to: "/press",
-        title: "Press",
-        component: PressReleases
-    },
+	clients: {
+		to: "/clients",
+		title: "Clients",
+		component: OurClients,
+	},
 
-    clients: {
-        to: "/clients",
-        title: "Clients",
-        component: OurClients
-    },
+	stock: {
+		to: "/stock",
+		title: "Stock",
+		component: StockPage,
+	},
 
-    stock: {
-        to: "/stock",
-        title: "Stock",
-        component: StockPage
-    },
-
-    // Keep last!
-    main: {
-        to: "/",
-        title: "Main",
-        component: MainPage
-    }
-}
+	// Keep last!
+	main: {
+		to: "/",
+		title: "Main",
+		component: MainPage,
+	},
+};
 
 const NAV_BAR_DATA = [
-    LINKS_DATA.stock,
-    LINKS_DATA.press,
-    LINKS_DATA.products,
-    LINKS_DATA.clients,
-    LINKS_DATA.team,
-    LINKS_DATA.about,
-    LINKS_DATA.contact
-]
+	LINKS_DATA.stock,
+	LINKS_DATA.press,
+	LINKS_DATA.products,
+	LINKS_DATA.clients,
+	LINKS_DATA.team,
+	LINKS_DATA.about,
+	LINKS_DATA.contact,
+];
 
-const NEWS_URI = `https://raw.githubusercontent.com/kanasoft-news/news/main/news.json`
-const STORY_URI = `https://tinyurl.com/yg5dbzky`
+const NEWS_URI = `https://raw.githubusercontent.com/kanasoft-news/news/main/news.json`;
+const YAHOO_FINANCE_PRESSES =
+	"https://webapi-backup.thecse.com/trading/listed/sedar_filings/00050734.json";
+const DOCUMENT_CATEGORIES = ["News Releases"];
+const STORY_URI = `https://tinyurl.com/yg5dbzky`;
 
-const TRACKING_ID = "UA-195443225-1"
+const TRACKING_ID = "UA-195443225-1";
 
 export {
-    COMPANY_NAME,
-    NAV_BAR_DATA,
-    LINKS_DATA,
-    NEWS_URI,
-    STORY_URI,
-    TRACKING_ID
-}
+	COMPANY_NAME,
+	NAV_BAR_DATA,
+	LINKS_DATA,
+	NEWS_URI,
+	YAHOO_FINANCE_PRESSES,
+	DOCUMENT_CATEGORIES,
+	STORY_URI,
+	TRACKING_ID,
+};
